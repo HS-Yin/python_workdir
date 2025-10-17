88e889f90423eb560005f4ca6f2c7862ef237ea2
HS-Yin <1468262779@qq.com>
2025-10-17 15:09:03 +0800

    test submit

--- 02_XiangQi/logic.py
+++ 02_XiangQi/logic.py
@@ -9,14 +9,3 @@ class XiangQi:
         self.winner = None
         self.move_history = []
         
-    def reset_board(self):
-        self.board = np.zeros((10, 9), dtype=int)
-        # Initialize the pieces on the board
-        self.board[0, :] = [1, 2, 3, 4, 5, 6, 7, 8, 9]  # Red pieces
-        self.board[9, :] = [-1, -2, -3, -4, -5, -6, -7, -8, -9]  # Black pieces
-        self.board[2, 1::2] = 10  # Red pawns
-        self.board[7, 1::2] = -10  # Black pawns
-        self.board[0, 3] = self.board[0, 5] = 11  # Red cannons
-        self.board[9, 3] = self.board[9, 5] = -11  # Black cannons
-        self.board[0, 1] = self.board[0, 7] = 12  # Red horses
-        self.board[9, 1] = self.board[9, 7] = -12  # Black horses
